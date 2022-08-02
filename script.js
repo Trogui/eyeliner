c40b0300eaf238bf559b71f617d4917aa4acf5a3
Trogui <ardillamelocoton@gmail.com>
2022-08-02 15:07:31 +0200

    1st Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("FormButton").onclick = function () {
+    location.href = "Form.html";
+};
+
+document.getElementById("IndexButton").onclick = function () {
+    location.href = "index.html";
+};
+
+document.getElementById("ColorButton").onclick = function () {
+    location.href = "Color.html";
+};
+
+function ColorChange () {
+    location.href = "Color.html"
+}
